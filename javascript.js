433729788b97a4fba26bfea210761081402f2d72
UW Portal SDK <aneville@uwaterloo.ca>
2015-03-25 15:48:58 -0400

    Updated: javascript.js on 3/25/2015 3:48:58 PM

--- javascript.js
+++ javascript.js
@@ -1,17 +1,14 @@
 angular.module('PortalApp')
 
-.controller('widgetCtrl', ['$scope', '$http', '$q', function ($scope, $http, $q) {
+.controller('widgetCtrl', ['$scope', '$http', '$q', '$filter', function ($scope, $http, $q, $filter) {
 
     // SETUP
 
     // Widget Configuration
     $scope.portalHelpers.config = {
-        "title": "Test Project",
-        "icon": "icon-bell"
+        "title": "Public Health Inspections",
+        "icon": "icon-attention"
     };
-
-    // Initialize input variable
-    $scope.insertValue = { value: "" };
 
     // Show loading message in the first column
     $scope.portalHelpers.showView('loading.html', 1);
@@ -19,75 +16,60 @@ angular.module('PortalApp')
     // Show loading animation
     $scope.portalHelpers.toggleLoading(true);
 
-    // DATABASE EXAMPLE
-
-    $scope.getDbData = function () {
-        $scope.portalHelpers.invokeServerFunction('getData').then(function (result) {
-            $scope.dbData = result;
-        });
+    /**
+     * Configuration
+     */
+    $scope.filter = { value: '' };
+  	$scope.itemsPerPage = 20;
+    $scope.currentPage = 1;
+    $scope.maxSize = 5;
+    $scope.matchingFacilities = 0;
+  	$scope.pageChanged = function(newPage) {
+        $scope.currentPage = newPage;
+        $scope.filterFacilities();
     }
-
-    // Try to get test data from the database
-    $scope.getDbData();
-
-    // Create table
-    $scope.createTable = function () {
-        $scope.portalHelpers.invokeServerFunction('createTable').then(function (result) {
-            $scope.getDbData();
-        });
+  
+    /**
+     * Facilities
+     */
+	$scope.getInspectionFacilities = function() {
+		$scope.portalHelpers.invokeServerFunction('getInspectionFacilities').then(function(result) {
+            $scope.facilities = $filter('orderBy')(result, 'name');
+            $scope.matchingFacilities = $scope.facilities;
+            $scope.filterFacilities();
+            $scope.portalHelpers.toggleLoading(false);
+            $scope.portalHelpers.showView('main.html', 1);
+        });   
+    }// End of getInspectionFacilities
+    
+    $scope.filterFacilities = function() {
+        console.log($scope.filter);
+        if ($scope.filter.value == '' || $scope.filter.value == $scope.lastFilterValue) {
+        	var start = $scope.itemsPerPage * ($scope.currentPage - 1);
+        	$scope.filteredFacilities = $scope.matchingFacilities.slice(start, start + $scope.itemsPerPage);
+        } else {
+            $scope.lastFilterValue = $scope.filter.value;
+            $scope.matchingFacilities = $filter('filter')($scope.facilities, $scope.filter.value);
+            $scope.filterFacilities();
+        }// End of else
+    }// End of filterFacilities
+    
+    $scope.resetFilterFacilities = function() {
+       $scope.filter.value = "";
+       $scope.matchingFacilities = $scope.facilities;
+       $scope.filterFacilities();
     }
-
-    // Insert a value into the database
-    $scope.insertData = function () {
-        if ($scope.insertValue.value.length > 50)
-            alert('value should be less than 50 characters');
-        else {
-            $scope.portalHelpers.invokeServerFunction('insert', { value: $scope.insertValue.value }).then(function (result) {
-                $scope.dbData = result;
-            });
-        }
-    };
-
+    
+    $scope.getInspectionFacilities();
+   
     // DETAILS VIEW EXAMPLE
-    $scope.showView2 = function () {
-        $scope.portalHelpers.showView('view2.html', 2);
+    $scope.showFacility = function (facility) {
+        $scope.portalHelpers.toggleLoading(true);
+        $scope.portalHelpers.showView('loading.html', 2);
+        $scope.portalHelpers.invokeServerFunction('getFacility', { facilityId: facility.id }).then(function(result) {
+            $scope.facility = result;
+            $scope.portalHelpers.toggleLoading(false);
+            $scope.portalHelpers.showView('facility.html', 2);
+        });
     }
-
-    $scope.showView3 = function () {
-        $scope.portalHelpers.showView('view3.html', 3);
-    }
-
-    // PORTAL DATA SOURCE EXAMPLE
-
-    // Get data for the widget
-    $http.get('/ImportantLinks/JSONSource').success(function (data) {
-        // Make data available in the scope
-        $scope.links = data;
-        // Turn off loading animation
-        $scope.portalHelpers.toggleLoading(false);
-        // Show main view
-        $scope.portalHelpers.showView('main.html', 1);
-    });
-
-    // OPEN API EXAMPLE
-    $scope.portalHelpers.invokeServerFunction('getOpenData').then(function (result) {
-        $scope.openDataExampleData = result;
-    });
-
-
 }])
-// Custom directive example
-.directive('DirectiveName', ['$http', function ($http) {
-    return {
-        link: function (scope, el, attrs) {
-
-        }
-    };
-}])
-// Custom filter example
-.filter('FilterName', function () {
-    return function (input, arg1, arg2) {
-        var output = input;
-        return output;
-    }
-});
